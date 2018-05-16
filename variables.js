76275a0c194a9c93299ff48225164cd314746192
Devin Bundrent <devin.bundrent@gmail.com>
2018-05-16 19:20:40 +0000

    automatically backed up by learn

---
+++ variables.js
@@ -0,0 +1,27 @@
+// declare a variable
+var word
+
+// assign a value to the variable
+word = 'bird'
+
+// print out the contents of the variable to the console
+console.log(word)
+
+// assign a new value to the variable
+word = 'dog'
+
+//print out the new value
+console.log(word)
+
+// Multi-Line variable assignment
+var a = 5, 
+    b = 2, 
+    c = 3, 
+    d = 'hello', 
+    e = 'goodbye';
+    
+  console.log(a)
+  console.log(b)
+  console.log(c)
+  console.log(d)
+  console.log(e)
